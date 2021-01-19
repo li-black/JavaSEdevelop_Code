2e27653795459a9ec8c2b4723e176fdecbf9f08e
li-black <1436988790@qq.com>
2021-01-19 10:54:44 +0800

    Revert "itheima jy eighteenth commit"
    This reverts commit 93251719

--- JavaSEdevelop_Code/day17/src/com/itheima/annotation/Anno1.java
+++
@@ -1,6 +0,0 @@
-package com.itheima.annotation;
-
-//自定义注解类
-public @interface Anno1 {
-    String name();
-}
--- JavaSEdevelop_Code/day17/src/com/itheima/enumeration/Test1.java
+++
@@ -1,6 +0,0 @@
-package com.itheima.enumeration;
-
-//枚举类
-public enum Test1 {
-    Spring, Summer, Autumn, Winter;
-}
