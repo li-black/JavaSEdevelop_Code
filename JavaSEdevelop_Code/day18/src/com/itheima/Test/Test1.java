279fbb224124762b0bee19a2334f81063e634976
li-black <1436988790@qq.com>
2021-01-19 18:09:38 +0800

    itheima jy eighteenth commit

---
+++ JavaSEdevelop_Code/day18/src/com/itheima/Test/Test1.java
@@ -0,0 +1,23 @@
+package com.itheima.Test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Scanner;
+
+public class Test1 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Test1.class);
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String number = sc.next();
+        try {
+            int result = Integer.parseInt(number);
+            LOGGER.info("类型转换成功" + result);
+//            System.out.println("转换成功");
+        } catch (NumberFormatException e) {
+            LOGGER.error("类型转换失败"+re);
+//            System.out.println("转换失败");
+        }
+    }
+}
---
+++ JavaSEdevelop_Code/day18/src/com/itheima/junit/Test1.java
@@ -0,0 +1,20 @@
+package com.itheima.junit;
+
+//方法类
+public class Test1 {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int mut(int a, int b) {
+        return a * b;
+    }
+
+    public int dev(int a, int b) {
+        return a / b;
+    }
+}
